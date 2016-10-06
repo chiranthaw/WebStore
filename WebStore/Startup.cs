5ce8f8fd4398e4feb06aa892c4858d4ceb2f6102
mihaivram96 <mihaiavram96@yahoo.com>
2016-10-06 12:50:00 +0200

    Commented on startup.cs

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -7,6 +7,8 @@ namespace WebStore
     public partial class Startup {
         public void Configuration(IAppBuilder app) {
             ConfigureAuth(app);
+
+            // Comment by Mihai 06.oct.2016
         }
     }
 }
